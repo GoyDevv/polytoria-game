455f0df015feb3065a60f5de459eaa4a1b8adc33
GoyDevv <276701144+GoyDevv@users.noreply.github.com>
2026-05-12 23:03:20 +0000

    Fix Android assemblies not found, api 35, and ci zipalign

--- test_godot_export.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-wget -q https://github.com/godotengine/godot/releases/download/4.2.2-stable/Godot_v4.2.2-stable_mono_linux_x86_64.zip
-unzip -q Godot_v4.2.2-stable_mono_linux_x86_64.zip
-chmod +x Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64
-cd Polytoria
-../Godot_v4.2.2-stable_mono_linux_x86_64/Godot_v4.2.2-stable_mono_linux.x86_64 --headless --export-release "Android" ../build/polytoria.apk > ../godot_log.txt 2>&1
